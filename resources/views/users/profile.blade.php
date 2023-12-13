fdb563f8963edbb008e3443958932456a9b7864a
春山 <>
2023-12-13 12:23:01 +0900

    既存情報を表示
    プロフィール画面に、既存の登録情報を表示させる

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,7 +1,43 @@
 @extends('layouts.login')
 
 @section('content')
-
+<div class="contaner">
+    <div class="update">
+        {!! Form::open(['url'=>'/profile/update'])!!}
+        @csrf
+        {{Form::hidden('id',Auth::user()->id)}}
+        <img class="update-icon" src="images/icon1.png">
+        <div class="update-form">
+            <div class="update-block">
+                <label for="name">user name</label>
+                <input type="text" name="username" value="{{Auth::user()->username}}">
+            </div>
+            <div class="update-block">
+                <label for="mail">mail address</label>
+                <input type="email" name="mail" value="{{Auth::user()->mail}}">
+            </div>
+            <div class="update-block">
+                <label for="pass">password</label>
+                <input type="password" name="password" value="{{Auth::user()->password}}">
+            </div>
+            <div class="update-block">
+                <label for="confirm-pass">password confirm</label>
+                <input type="password" name="confirm_password" value="{{Auth::user()->password}}">
+            </div>
+            <div class="update-block">
+                <label for="name">bio</label>
+                <input type="text" name="bio" value="{{Auth::user()->bio}}">
+            </div>
+            <div class="update-block">
+                <label for="name">icon image</label>
+                <input type="file" name="images">
+            </div>
+            <input type="submit" class="btn btn-danger">
+            {{Form::token()}}
+            {!! Form::close()!!}
+        </div>
+    </div>
+</div>
 
 
 @endsection
