8e08987f877acc6eeae3a18c7bc995d51907c437
春山 <>
2023-12-29 23:25:10 +0900

    プロフィールの編集機能実装

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -12,5 +13,26 @@ class UsersController extends Controller
     }
     public function search(){
         return view('users.search');
+    }
+
+    // プロフィール編集機能
+    public function update(Request $request)
+    {
+        $id = $request->input('id');
+        $username = $request->input('username');
+        $mail = $request->input('mail');
+        $password = $request->input('password');
+        $bio = $request->input('bio');
+        $icon = $request->input('images');
+
+        User::where('id',$id)->update([
+            'username' => $username,
+            'mail' => $mail,
+            'password' => $password,
+            'bio' => $bio,
+            // 'images' => $icon,
+        ]);
+
+       return redirect('/top');
     }
 }
