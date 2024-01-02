10af514e3938f2bdb92f4abdab68e3b4205899db
春山 <>
2024-01-02 19:20:26 +0900

    投稿フォームの設置

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>機能を実装していきましょう。</h2>
+<div class="contaner">
+    <h2>機能を実装していきましょう。</h2>
+<div class="post">
+    <input type="image" name="images" value="{{Auth::user()->images}}">
+    <from action = "/" meshod="post"></from>
+    <input type = "text">
+    <div class = "button">
+        <button type = "submit"><img class="post-btn" src="images/post.png"></button>
+</div>
+</div>
+
+</div>
+
 
 @endsection
