a1d93d4a4b0677a4839994315cee216cdfd22bcb
春山 <>
2024-02-05 03:18:28 +0900

    アイコンの表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,13 +4,13 @@
 <div class="contaner">
     {!! Form::open(['url' => '/posts/index']) !!}
 <div class="post">
-<input type="image" name="images" value="{{Auth::user()->images}}">
+<input type="image" name="images" value=""><img src="{{ asset('storage/'.Auth::user()->images) }}">
     {!! Form::input('text','newPost',null,['required','class' => 'form-control','placeholder' => '投稿内容を入力してください'])!!}
     <div class = "button">
         <button type = "submit"><img class="post-btn" src="images/post.png"></button>
     </div>
 </div>
-{!! Form::close() !!{}}
+{!! Form::close() !!}
 
 </div>
 
