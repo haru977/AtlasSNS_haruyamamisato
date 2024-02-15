6306d7d2902fb12abbc866c6200621dbd8fb5053
春山 <>
2024-02-16 06:46:13 +0900

    アイコン、送信ボタンのサイズ調整

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,10 +4,10 @@
 <div class="contaner">
     {!! Form::open(['url' => '/posts/index']) !!}
 <div class="post">
-<input type="image" name="images" value=""><img src="{{ asset('storage/'.Auth::user()->images) }}">
+<input type="image" name="images" value=""><img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
     {!! Form::input('text','newPost',null,['required','class' => 'form-control','placeholder' => '投稿内容を入力してください'])!!}
     <div class = "button">
-        <button type = "submit"><img class="post-btn" src="images/post.png"></button>
+        <button type = "submit"><img class="post-btn" src="images/post.png" width="25" height="25"></button>
     </div>
 </div>
 {!! Form::close() !!}
