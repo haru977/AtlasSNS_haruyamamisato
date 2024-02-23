6159419ef8e9293e10ec815648c62b74d05f90a0
春山 <>
2024-02-23 21:16:57 +0900

    投稿機能の作成
    投稿データの保存機能作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
-    //
-    public function index(){
-        return view('posts.index');
+
+public function index()
+{
+    // 投稿の表示
+    $posts = Post::all();
+
+    return view('posts.index', compact('posts'));
+}
+
+
+// 投稿機能
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'content' => 'required|max:155']);
+
+        $post = new Post();
+        $post->post = $request->input('content');
+        $post->user_id = auth()->user()->id;
+        $post->save();
+
+        return redirect('/top');
     }
 }
