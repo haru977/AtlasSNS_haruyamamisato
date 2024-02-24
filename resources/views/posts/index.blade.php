0dec43ceba08083369b57d0d45c6b5202ef1d927
春山 <>
2024-02-24 20:40:44 +0900

    ログインユーザーのアイコン表示コード修正

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,10 +2,12 @@
 
 @section('content')
 <div class="contaner">
-    {!! Form::open(['url' => '/posts/index']) !!}
+    {!! Form::open(['url' => '/posts']) !!}
 <div class="post">
-<input type="image" name="images" value=""><img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
-    {!! Form::input('text','newPost',null,['required','class' => 'form-control','placeholder' => '投稿内容を入力してください'])!!}
+<img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+<!-- <input type="image" name="images" value=""><img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25"> -->
+
+    {!! Form::text('content',null,['required','class' => 'form-control','placeholder' => '投稿内容を入力してください'])!!}
     <div class = "button">
         <button type = "submit"><img class="post-btn" src="images/post.png" width="25" height="25"></button>
     </div>
