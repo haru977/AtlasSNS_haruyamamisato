ef9883647c2fe018176c9f5ce039cdd48f5e569e
春山 <>
2024-02-27 00:24:22 +0900

    投稿者情報の受け取りのためのリレーション設定
    ＋バリデーション設定”1文字以上”を追記

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,16 +12,14 @@ public function index()
 {
     // 投稿の一覧表示
     $posts = Post::all();//投稿データを取得
-
     return view('posts.index', compact('posts'));//投稿データをビューに渡す
 }
-
 
 // 投稿機能
     public function store(Request $request)
     {
         $this->validate($request,[
-            'content' => 'required|max:155']);
+            'content' => 'required|min:1|max:155']);
 
         $post = new Post();
         $post->post = $request->input('content');//postカラムにデータを保存
@@ -29,5 +27,12 @@ public function index()
         $post->save();
 
         return redirect('/top');
+    }
+
+    //投稿者情報の受け取りのためリレーション設定
+    //1対多の「1」と結合（一人の投稿者が複数の投稿をすることができる）
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
