42d8aba0ad70852f6555bc740f73ef61355b0516
春山 <>
2024-03-05 10:13:53 +0900

    リレーション設定の追記

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+
+    //投稿者情報の受け取りのためリレーション設定
+    //1対多の「1」と結合（一人の投稿者が複数の投稿をすることができる）
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
