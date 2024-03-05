088bf1df9bef5391284118cf27c8666d00eb826a
春山 <>
2024-03-05 10:13:29 +0900

    リレーションコードの移動（post.php モデルへ移動）

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -29,10 +29,4 @@ public function index()
         return redirect('/top');
     }
 
-    //投稿者情報の受け取りのためリレーション設定
-    //1対多の「1」と結合（一人の投稿者が複数の投稿をすることができる）
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
