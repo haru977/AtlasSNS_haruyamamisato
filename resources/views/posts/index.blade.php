91f1c0654f18246408d7ae42520c63272281e6fd
春山 <>
2024-03-07 20:06:16 +0900

    編集、削除ボタンの設置＆モーダルのビューを記載

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,28 +3,45 @@
 @section('content')
 <div class="container">
     {!! Form::open(['url' => '/posts']) !!}
-<div class="post">
-<img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
-<!-- <input type="image" name="images" value=""><img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25"> -->
-
-    {!! Form::text('content',null,['required','class' => 'form-control','placeholder' => '投稿内容を入力してください'])!!}
-    <div class = "button">
-        <button type = "submit"><img class="post-btn" src="images/post.png" width="25" height="25"></button>
+    <div class="post">
+        <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+        {!! Form::text('content',null,['required','class' => 'form-control','placeholder' => '投稿内容を入力してください'])!!}
+        <div class = "button">
+            <button type = "submit"><img class="post-btn" src="images/post.png" width="25" height="25"></button>
+        </div>
     </div>
-</div>
 {!! Form::close() !!}
-
 <div class="posts">
     <h2>投稿機能</h2>
     <ul>
     <!-- 投稿内容一覧表示のためのループを開始↓ -->
         @foreach ($posts as $post)
+        <li>
+        <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
         <!-- postカラムの値（投稿内容）を表示↓ -->
-        <li>><strong>{{ $post->user->name }}</strong>: {{ $post->post }}</li>
+        <strong>{{ $post->user->username }}</strong>: {{ $post->post }}
+        <span>{{ $post->created_at->format('Y-m-d H:i:s') }}</span>
+        <button type = "button" class="js-modal-open" href="" post="{{ $post->post }}" post_id="{{ $post->id }}">
+            <img class="edit-btn" src="images/edit.png" width="25" height="25"></button>
+        <button type = "submit">
+            <img class="delete-btn" src="images/trash.png" width="25" height="25"></button>
+    </li>
         @endforeach
+        <!-- モーダルの中身 -->
+    <div class="modal js-modal">
+        <div class="modal__bg js-modal-close"></div>
+        <div class="modal__content">
+           <form action="" method="">
+                <textarea name="" class="modal_post"></textarea>
+                <input type="text" name="" class="modal_id" value="">
+                <input type="submit" value="更新">
+                {{ csrf_field() }}<!-- CSRF保護機能 -->
+           </form>
+           <a class="js-modal-close" href="">閉じる</a>
+        </div>
+    </div>
     </ul>
 </div>
 </div>
-
 
 @endsection
