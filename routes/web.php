b27644e95def8ebe1e1109f111c142881e5e5e50
春山 <>
2024-03-08 15:01:27 +0900

    投稿の削除機能ルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,11 @@ Route::post('/profile/update','UsersController@update');// プロフィール編
 Route::post('/posts','PostsController@store');// 投稿データ送信
 Route::get('/posts','PostsController@index')->name("posts,index");//投稿データ表示
 
+// Route::post('/top','PostsController@update');// 投稿の編集
+// Route::get('/posts','PostsController@update');// 投稿の編集
+
+Route::get('/post/{id}/delete','PostsController@delete');// 投稿の削除
+
 Route::get('/search','UsersController@index');
 
 Route::get('/follow-list','PostsController@index');
