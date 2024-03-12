5ee2151b1c3e6957035e4d5386e8036ee9c0d6d1
春山 <>
2024-03-12 23:41:59 +0900

    検索機能のビュー記述

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.login')
 
 @section('content')
-
+<!-- 検索機能 -->
+<div class="container">
+    <form action="/search">
+        @csrf
+        <input type="text" name="keyword" class="form" placeholder="ユーザー検索">
+        <button type="submit" class="btn btn-success">検索</button>
+    </form>
+    <div class=search-user>
+    @foreach ($users as $user)
+    <tr>
+        <td>{{ $user->username }}</td>
+    <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+    </tr>
+    @endforeach
+    </div>
+</div>
 
 
 @endsection
