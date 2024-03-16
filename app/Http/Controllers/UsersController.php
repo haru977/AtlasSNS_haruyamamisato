5f96b2bbc5d5e4ddd0afea90e7b2318738ae0539
春山 <>
2024-03-16 17:44:13 +0900

    検索ワードの表示
    ＄keywordにセットされたワードを渡す処理

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -26,7 +26,7 @@ class UsersController extends Controller
         }else{
             $users = User::all();
         }
-        return view('users.search',['users'=>$users]);
+        return view('users.search',['users'=>$users, 'keyword' => $keyword]);
     }
 
     // プロフィール編集機能
