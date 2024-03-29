7d0353a4482a7070a22067aeb331d1e5f07ab6a7
春山 <>
2024-03-29 12:35:14 +0900

    フォローリスト　ビューの設置

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.login')
 
 @section('content')
+<div class="container">
+    <h1>フォローしているユーザーの投稿</h1>
+        <ul class="list-group">
+            @foreach($posts as $post)
+                <li class="list-group-item">
+                    <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+                    <strong>{{ $post->user->username }}</strong>: {{ $post->post }}
+                    <span>{{ $post->created_at->format('Y-m-d H:i:s') }}</span>
 
+                </li>
+            @endforeach
+        </ul>
+</div>
 @endsection
