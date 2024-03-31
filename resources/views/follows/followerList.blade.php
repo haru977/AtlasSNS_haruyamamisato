4adf293e638c4b39e40860aeed4a9cbde9ec7158
春山 <>
2024-03-31 22:23:51 +0900

    フォロワーリスト ビュー

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -1,4 +1,19 @@
 @extends('layouts.login')
-
 @section('content')
+<div class="container">
+    @if(count($posts) > 0)
+        <ul class="list-group">
+            @foreach($posts as $post)
+                <li class="list-group-item">
+                    <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+                    <strong>{{ $post->user->username }}</strong>: {{ $post->post }}
+                    <span>{{ $post->created_at->format('Y-m-d H:i:s') }}</span>
+
+                </li>
+            @endforeach
+        </ul>
+    @else
+    @endif
+</div>
+
 @endsection
