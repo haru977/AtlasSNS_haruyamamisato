496fc2b59a90340e989a9956f66e4beba979e59e
春山 <>
2024-05-03 02:22:20 +0900

    アイコンにユーザーページへのリンクを設置

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -7,7 +7,9 @@
         <ul class="list-group">
             @foreach($posts as $post)
                 <li class="list-group-item">
+                    <a class="other-profile" href="{{ route('other.profile' , ['id' => $post->user->id]) }}"><!-- 相手のプロフィールページへの遷移 -->
                     <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+                    </a>
                     <strong>{{ $post->user->username }}</strong>: {{ $post->post }}
                     <span>{{ $post->created_at->format('Y-m-d H:i:s') }}</span>
 
