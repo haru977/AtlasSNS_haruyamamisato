b2e3db0866733adb82749d3e8b6eb85af76af224
春山 <>
2024-06-10 22:32:56 +0900

    フォローリストの作成

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.login')
 
 @section('content')
+<!-- フォローリスト -->
 <div class="container">
-    <!-- フォローリスト -->
-    @if(count($posts) > 0)
+    <!-- フォローユーザーアイコンの一覧表示 -->
+    <div class="icon-list">
+        @foreach($posts->unique('user_id') as $post)<!-- 重複するユーザーの排除(unique) -->
+        <a class="other-icon" href="{{ route('other.profile' , ['id' => $post->user->id]) }}">
+            <img src="{{ asset('storage/' . $post->user->images) }}" alt="{{ $post->user->username }}'s Icon" width="25" height="25">
+        </a>
+        @endforeach
+        <!-- フォローユーザーの一覧表示 -->
+        @foreach($posts as $post)
         <ul class="list-group">
-            @foreach($posts as $post)
                 <li class="list-group-item">
                     <a class="other-profile" href="{{ route('other.profile' , ['id' => $post->user->id]) }}"><!-- 相手のプロフィールページへの遷移 -->
-                    <img src="{{ asset('storage/'.Auth::user()->images) }}" width="25" height="25">
+                    <img src="{{ asset('storage/'. $post->user->images) }}" width="25" height="25">
                     </a>
-                    <strong>{{ $post->user->username }}</strong>: {{ $post->post }}
+                    <strong>{{ $post->user->username }}</strong>:
+                    {{ $post->post }}
                     <span>{{ $post->created_at->format('Y-m-d H:i:s') }}</span>
-
                 </li>
-            @endforeach
         </ul>
-    @else
-    @endif
+        @endforeach
+    </div>
 </div>
 @endsection
