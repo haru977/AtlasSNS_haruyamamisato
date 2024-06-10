0b6377d19ba8b294a52c6d110cb1c5e0f61c1f39
春山 <>
2024-06-10 22:27:33 +0900

    フォロワーリストの作成

--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -2,21 +2,27 @@
 @section('content')
 <!-- フォロワーリスト -->
 <div class="container">
-    @if(count($posts) > 0)
-        <ul class="list-group">
+    <!-- フォロワーアイコンの一覧表示 -->
+    <div class="icon-list">
+        @foreach($posts->unique('user_id') as $post)<!-- 重複するユーザーの排除'unique' -->
+        <a class="other-icon" href="{{ route('other.profile' , ['id' => $post->user->id]) }}">
+            <img src="{{ asset('storage/' . $post->user->images) }}" alt="{{ $post->user->username }}'s Icon" width="25" height="25">
+        </a>
+        @endforeach
+        <!-- フォロワーの一覧表示 -->
             @foreach($posts as $post)
+            <ul class="list-group">
                 <li class="list-group-item">
                     <a class="other-profile" href="{{ route('other.profile' , ['id' => $post->user->id]) }}"><!-- 相手のプロフィールページへの遷移 -->
                     <img src="{{ asset('storage/' . $post->user->images) }}" width="25" height="25">
                     </a>
-                    <strong>{{ $post->user->username }}</strong>: {{ $post->post }}
+                    <strong>{{ $post->user->username }}</strong>:
+                    {{ $post->post }}
                     <span>{{ $post->created_at->format('Y-m-d H:i:s') }}</span>
-
                 </li>
             @endforeach
         </ul>
-    @else
-    @endif
+    </div>
 </div>
 
 @endsection
