01abdfa5197a9b3e099a951d367fb850c6770243
春山 <>
2024-06-29 22:41:33 +0900

    ログイン画面HTML修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,19 @@
 @section('content')
 <!-- 適切なURLを入力してください -->
 {!! Form::open(['url' => '/login']) !!}
-
-<p>AtlasSNSへようこそ</p>
-
-{{ Form::label('e-mail') }}
-{{ Form::text('mail',null,['class' => 'input']) }}
-{{ Form::label('password') }}
-{{ Form::password('password',['class' => 'input']) }}
-
-{{ Form::submit('ログイン') }}
-
-<p><a href="/register">新規ユーザーの方はこちら</a></p>
+<div class="login-container">
+    <div class="login-block">
+        <p class="home">AtlasSNSへようこそ</p>
+        <div class="login-form">
+            <div class="login-form-mail">{{ Form::label('メールアドレス') }}{{ Form::text('mail',null,['class' => 'input']) }}</div>
+            <div class="login-form-pass">{{ Form::label('パスワード') }}{{ Form::password('password',['class' => 'input']) }}</div>
+            <div class="login-btn">
+            <button type="input" class="btn btn-danger">{{ Form::submit('ログイン') }}</button>
+            </div>
+            <a href="/register"><p>新規ユーザーの方はこちら</p></a>
+        </div>
+    </div>
+</div>
 
 {!! Form::close() !!}
 
