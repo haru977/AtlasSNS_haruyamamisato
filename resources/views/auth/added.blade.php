b26e3c92ad1c58893bf3df6266d31fe363251850
春山 <>
2024-08-14 13:05:51 +0900

    登録完了画面のレイアウト修正
    HTML＆CSS設定

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -2,14 +2,17 @@
 
 @section('content')
 
-<div id="clear">
-  <!-- 新規登録したユーザー名をセッションで取得 -->
-  <p>{{session('username')}}さん</p>
-  <p>ようこそ！AtlasSNSへ！</p>
-  <p>ユーザー登録が完了しました。</p>
-  <p>早速ログインをしてみましょう。</p>
-
-  <p class="btn"><a href="/login">ログイン画面へ</a></p>
+<div class="added-container" id="clear">
+  <div class="added">
+    <!-- 新規登録したユーザー名をセッションで取得 -->
+    <p class="added-text">{{session('username')}}さん</p>
+    <p class="added-text">ようこそ！AtlasSNSへ！</p><br>
+    <p class="added-text">ユーザー登録が完了しました。</p>
+    <p class="added-text">早速ログインをしてみましょう!</p><br>
+    <div class="added-btn">
+    <button type="input" class="btn btn-danger"><a href="/login">ログイン画面へ</a></button>
+    </div>
+  </div>
 </div>
 
 @endsection
