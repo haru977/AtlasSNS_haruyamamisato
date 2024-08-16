154d034dadd0d10705680c3794db547debe057fe
春山 <>
2024-08-16 21:25:52 +0900

    パスワードの非表示設定

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,22 +5,24 @@
 {!! Form::open(['url' => '/register']) !!}
 <div class="register-container">
     <div class="register-block">
-    <p class="register">新規ユーザー登録</p>
+        <p class="register">新規ユーザー登録</p>
         <div class="username">{{ Form::label('ユーザー名') }}
         {{ Form::text('username',null,['class' => 'input']) }}
-    </div>
-    <div class="mail">{{ Form::label('メールアドレス') }}
+        </div>
+        <div class="mail">{{ Form::label('メールアドレス') }}
         {{ Form::text('mail',null,['class' => 'input']) }}
-    </div>
-    <div class="password">{{ Form::label('パスワード') }}
-        {{ Form::text('password',null,['class' => 'input']) }}
-    </div>
-    <div class="password-confirmation">{{ Form::label('パスワード確認') }}
-        {{ Form::text('password_confirmation',null,['class' => 'input']) }}
-    </div>
-    <div class="submit">
-        <button type="input" class="btn btn-danger">{{ Form::submit('新規登録') }}</button></div>
-    <a class="register" href="/login">ログイン画面に戻る</a>
+        </div>
+        <div class="password">{{ Form::label('パスワード') }}
+        {{ Form::password('password',null,['class' => 'input']) }}
+        </div>
+        <div class="password-confirmation">{{ Form::label('パスワード確認') }}
+        {{ Form::password('password_confirmation',null,['class' => 'input']) }}
+        </div>
+        <div class="submit">
+        <button type="input" class="btn btn-danger">{{ Form::submit('新規登録') }}</button>
+        </div>
+        <div class="login-page"><a class="register" href="/login">ログイン画面に戻る</a>
+        </div>
     </div>
 </div>
 
